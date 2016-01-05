23ccf6608494882c822771219d878d31dcacbbea
Ramires Gomes <ramires.gb@gmail.com>
2016-01-05 19:36:26 -0200

    Trabalhando com exec() - query()

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 
 try {
     $conexao = new \PDO("mysql:host=localhost;dbname=pdo","root","root");
+
+    //$query = "insert into clientes(nome,email) values ('Pedro', 'predro@gmail.com'), ('Joao', 'joao@gmail.com')";
+    $query = "Select * from clientes;";
+
+    $stmt = $conexao->query($query);
+    $resultado = $stmt->fetchAll();
+
+    echo $resultado[0]['nome'];
+
+    //print_r( $resultado );
 }
 catch(\PDOException $e) {
     echo "Não foi possível estabelecer a conexão com o banco de dados. Erro código:".$e->getCode().": ".$e->getMessage();
